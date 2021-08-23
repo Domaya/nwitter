b1939f14f0a18bb4e05686b84d569c4cf447f218
Domaya <c2hila@gmail.com>
2021-08-23 23:33:32 +0900

    Add social LOGIN

--- src/fbase.js
+++ src/fbase.js
@@ -20,5 +20,7 @@ const firebaseConfig = {
 }
 firebase.initializeApp(firebaseConfig);
 
+export const firebaseInstance = firebase;
+
 export const authService = firebase.auth();
 // export default authService;
