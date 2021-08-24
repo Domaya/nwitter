d554d8c16d80517e21ff8c1b98a5faaae3cb3339
Domaya <c2hila@gmail.com>
2021-08-24 22:24:04 +0900

    Update nweet

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,28 +1,112 @@
+import React, { useState, useEffect } from "react";
 import { dbService } from "fbase";
-import React, { useState } from "react";
 
 const Home = () => {
-  const onSubmit = async(event) =>{
+  const [nweet, setNweet] = useState("");
+  const [nweets, setNweets] = useState([]);
+  const getNweets = async () => {
+    const dbNweets = await dbService.collection("nweets").get();
+    dbNweets.forEach((document) => {
+      const nweetObject = {
+        ...document.data(),
+        id: document.id,
+      };
+      setNweets((prev) => [nweetObject, ...prev]);
+    });
+  };
+  useEffect(() => {
+    getNweets();
+  }, []);
+  const onSubmit = async (event) => {
     event.preventDefault();
     await dbService.collection("nweets").add({
-        nweet,
-        createdAt : Date.now(),
-      })
-      SetNweet("");
-  }
-  const [nweet, SetNweet] = useState("");
-  const onChange = (event) =>{
-    const { target:{value}} = event;
-    SetNweet(value);
-  }
+      nweet,
+      createdAt: Date.now(),
+    });
+    setNweet("");
+  };
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setNweet(value);
+  };
+  console.log(nweets);
   return (
     <div>
       <form onSubmit={onSubmit}>
-        <input value={nweet} onChange={onChange} type="text" placeholder="What's on your mind?" maxLength={120}></input>
-        <input type="submit" value="Nweet"></input>
+        <input
+          value={nweet}
+          onChange={onChange}
+          type="text"
+          placeholder="What's on your mind?"
+          maxLength={120}
+        />
+        <input type="submit" value="Nweet" />
       </form>
+      <div>
+        {nweets.map((nweet) => (
+          <div key={nweet.id}>
+            <h4>{nweet.nweet}</h4>
+          </div>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
+// import { dbService } from "fbase";
+// import React, { useEffect, useState } from "react";
+
+// const Home = () => {
+//   const [nweet, SetNweet] = useState(""); //nweet 작성을 위한
+//   const [nweets, SetNweets] = useState(""); //nweet를 가져오기 위한
+//   const getNweets = async() => {
+//     const dbNweets = await dbService.collection("nweets").get();
+//     dbNweets.forEach((document) => {
+//       const nweetObject = {
+//         ...document.data(),
+//         id : document.id
+//       }
+//       SetNweets((prev) => [nweetObject, ...prev]);
+//     })
+//   }
+//   useEffect(() => {
+//     getNweets();
+//   }, [])
+//   const onSubmit = async(event) =>{
+//     event.preventDefault();
+//     await dbService.collection("nweets").add({
+//         nweet,
+//         createdAt : Date.now(),
+//       })
+//       SetNweet("");
+//   }
+//   const onChange = (event) =>{
+//     const { target:{value}} = event;
+//     SetNweet(value);
+//   }
+//   console.log(nweets);
+//   return (
+//     <div>
+//       <form onSubmit={onSubmit}>
+//         <input
+//           value={nweet}
+//           onChange={onChange}
+//           type="text"
+//           placeholder="What's on your mind?"
+//           maxLength={120}
+//         />
+//         <input type="submit" value="Nweet" />
+//       </form>
+//       <div>
+//         {nweets.map((nweet) => (
+//           <div key={nweet.id}>
+//             <h4>{nweet.nweet}</h4>
+//           </div>
+//         ))}
+//       </div>
+//     </div>
+//   )
+// }
 
 export default Home
