8eb0e6baefea1bd3f65447a0e2057ee55a4777db
Domaya <c2hila@gmail.com>
2021-08-29 01:09:56 +0900

    Change way to set UserObj

--- src/components/App.js
+++ src/components/App.js
@@ -10,17 +10,30 @@ function App() {
   useEffect(() => {
     authService.onAuthStateChanged((user) =>{
       if(user){
-        setIsLoggedIn(true);
-        setUserObj(user);
-      }else{
-        setIsLoggedIn(false);
+        setUserObj({
+          displayName: user.displayName,
+          uid: user.uid,
+          updateProfile : (args) => user.updateProfile(args),
+        });
       }
       setInit(true);
     })
   }, [])
+  const refreshUser = () => {
+    const user= authService.currentUser;
+    setUserObj({
+      displayName:user.displayName,
+      uid:user.uid,
+      updateProfile : (args) => user.updateProfile(args)
+    })
+  }
+
   return (
     <>
-      {init ? <AppRouter isLoggedIn={isLoggedIn} userObj={userObj} />: "Initializing..."}
+      {init ? <AppRouter 
+      refreshUser={refreshUser}
+      isLoggedIn={isLoggedIn} 
+      userObj={userObj} />: "Initializing..."}
       <footer>&copy; {new Date().getFullYear()} Dowitter </footer>
     </>); //props전달
 }
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -2,7 +2,7 @@ import { authService, dbService } from "fbase";
 import React, { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
 
-export default ( {userObj} ) => {
+export default ( {userObj, refreshUser} ) => {
   const history = useHistory();
   const [newDisplayName, setNewDisplayName] = useState(userObj.displayName);
   const onLogOutClick = () => {
@@ -10,17 +10,17 @@ export default ( {userObj} ) => {
     history.push("/");
   };
   
-  // const getMyNweets = async() => {
-  //   const nweets = await dbService
-  //   .collection("nweets")
-  //   .where("creatorId", "==", userObj.uid)
-  //   .orderBy("createdAt", "desc")
-  //   .get();
-  //   console.log(nweets.docs.map((doc) => doc.data()));
-  // }
-  // useEffect(() => {
-  //   getMyNweets();
-  // }, [])
+  const getMyNweets = async() => {
+    const nweets = await dbService
+    .collection("nweets")
+    .where("creatorId", "==", userObj.uid)
+    .orderBy("createdAt", "desc")
+    .get();
+    console.log(nweets.docs.map((doc) => doc.data()));
+  }
+  useEffect(() => {
+    getMyNweets();
+  }, [])
 
   const onChange = (event) => {
     const { target: {value} } = event;
@@ -32,6 +32,7 @@ export default ( {userObj} ) => {
       await userObj.updateProfile({
         displayName: newDisplayName
       });
+      refreshUser();
     }
   }
 
